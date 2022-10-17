6838ee55cde9b45d129dd7a6d8905299571649df
Sergei Sviridov <asgard998@yandex.ru>
2022-10-17 17:04:57 +0200

    2.4

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 Команда запуска проекта:
-yarn run dev
+yarn run dev 
+npm run dev
+
+Запуск express:
+npm start
 
 Ссылка на макет в Figma:
 https://www.figma.com/file/IMsXBqoz7otiAwdPLArSwx/Untitled?node-id=0%3A1
---
+++ express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(`${__dirname}/index/`));
+
+
+app.listen(PORT, () => {
+    console.log(`Мой порт: ${PORT}!`);
+});
