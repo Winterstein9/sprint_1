3cfd758e148913bc22638c1988e691e18f5ae2cb
Sergei Sviridov <asgard998@yandex.ru>
2022-10-17 18:53:24 +0200

    2.5

--- express.js
+++ express.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(`${__dirname}/index/`));
-
+app.use(express.static(`${__dirname}/dist/`));
 
 app.listen(PORT, () => {
     console.log(`Мой порт: ${PORT}!`);
